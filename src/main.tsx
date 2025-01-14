0092b29dc37830338bd7baf420f6dbbc584e7aba
Ruslan Yakushev <yarusl42@gmail.com>
2025-01-14 06:17:31 +0300

    .

--- src/main.tsx
+++ src/main.tsx
@@ -6,26 +6,27 @@ import { Provider } from 'react-redux'
 import { store } from './store'
 import { BrowserRouter } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import 'react-toastify/dist/ReactToastify.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-        <ToastContainer
-          position="top-right"
-          autoClose={3000}
-          hideProgressBar={false}
-          newestOnTop
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="light"
-        />
-      </BrowserRouter>
-    </Provider>
+    <GoogleOAuthProvider clientId="1062893175479-7q7k5q1j8q1j5q1j5q1j5q1j5q1j5q1j.apps.googleusercontent.com">
+      <Provider store={store}>
+          <App />
+          <ToastContainer
+            position="top-right"
+            autoClose={3000}
+            hideProgressBar={false}
+            newestOnTop
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="light"
+          />
+      </Provider>
+    </GoogleOAuthProvider>
   </React.StrictMode>,
 )
